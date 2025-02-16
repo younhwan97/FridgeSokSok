13caa71278df182ff0683306a1238bc0623bdeb2
younhwan97 <younhwan0903@gmail.com>
2025-02-16 19:41:38 +0900

    Add font

--- app/src/main/java/com/yh/fridgesoksok/ui/home/HomeScreen.kt
+++ app/src/main/java/com/yh/fridgesoksok/ui/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.yh.fridgesoksok.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -18,10 +20,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.yh.fridgesoksok.ui.theme.CustomBackGroundColor
 import com.yh.fridgesoksok.ui.theme.CustomPrimaryColor
+import com.yh.fridgesoksok.ui.theme.CustomTextColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,11 +36,15 @@ fun HomeScreen() {
             TopAppBar(
                 colors = topAppBarColors(
                     containerColor = CustomBackGroundColor,
-                    titleContentColor = CustomPrimaryColor,
+                    titleContentColor = CustomTextColor,
                 ),
                 title = {
-                    Text("")
-                }
+                    Text(
+                        text = "Fridge",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                },
+                modifier = Modifier.shadow(elevation = 2.dp)
             )
         },
         bottomBar = {
@@ -62,7 +71,11 @@ fun HomeScreen() {
     ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding),
+                .padding(innerPadding)
+                .background(color = CustomBackGroundColor)
+                .fillMaxWidth()
+                .fillMaxHeight()
+            ,
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             Text("hello")
--- app/src/main/java/com/yh/fridgesoksok/ui/theme/Type.kt
+++ app/src/main/java/com/yh/fridgesoksok/ui/theme/Type.kt
@@ -2,33 +2,31 @@ package com.yh.fridgesoksok.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.yh.fridgesoksok.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.pretendard_thin, FontWeight.Thin, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    bodySmall = TextStyle(
+        fontFamily = Pretendard, fontWeight = FontWeight.Normal, fontSize = 12.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    bodyMedium = TextStyle(
+        fontFamily = Pretendard, fontWeight = FontWeight.Normal, fontSize = 16.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Pretendard, fontWeight = FontWeight.Bold, fontSize = 20.sp
     )
-    */
+
 )
